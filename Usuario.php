6ecbf905be1cd0755d1d323781e809e3360f7c1d
JULIO-GARCÍA <a20200399@utem.edu.mx>
2023-06-07 19:12:53 -0600

    Creacion de login

---
+++ Usuario.php
@@ -0,0 +1,11 @@
+<?php
+   $user = $_GET['user'];
+   $password = $_GET['password'];
+   if (($user == "usuario") AND ($password == "12345")){
+      header('Location: Presentacion.html');
+   }
+   Else{
+      echo "¡Usuario o contraseña incorrectos!";
+      echo '<a href="'.$_SERVER['HTTP_REFERER'].'"><br>Volver</a>';
+   }
+?>
